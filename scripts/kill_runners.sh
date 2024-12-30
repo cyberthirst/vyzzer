4737996a99917a4ee3d15b9bb5d86c7bc28035cd
cyberthirst <cyberthirst.eth@gmail.com>
2024-12-30 18:52:53 +0100

    update scripts

--- scripts/kill_runners.sh
+++ scripts/kill_runners.sh
@@ -2,6 +2,5 @@
 
 pkill -f "python fuzz/runners/runner_nagini.py"
 pkill -f "python fuzz/runners/runner_ivy.py"
-pkill -f "./scripts/ram_monitor.sh"
 
 echo "Runners were killed"
--- scripts/ram_monitor.sh
+++ scripts/ram_monitor.sh
@@ -2,8 +2,28 @@
 
 source venv/bin/activate
 
+export PYTHONPATH=$(pwd)
+
+counter=0
+
 while true; do
     sleep 300
-    ./scripts/kill_runners.sh
-    ./scripts/run_runners.sh
+    ((counter++))
+    if [ $((counter % 4)) -eq 0 ]; then
+      ./scripts/kill_fuzzer.sh
+      ./scripts/start_fuzzer.sh
+    else
+      ./scripts/kill_runners.sh
+      ./scripts/run_runners.sh
+    fi
+    grep -nr Seed logs/generator.log >> logs/compiler_crash.txt
+    grep -nr Seed logs/generator.log >> logs/output_diff.txt
+    python fuzz/results/print_contracts_with_output_difference.py >> logs/output_diff.txt
+    python fuzz/results/print_contracts_with_compiler_error.py >> logs/compiler_crash.txt
+    if ! pgrep -f "fuzz/generators/run_diff_ivy.py" > /dev/null; then
+      python fuzz/generators/run_diff.py
+    fi
+done
+
+
 done
